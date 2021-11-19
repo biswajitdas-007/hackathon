bd2fccd30f78c86f8be60898d02c2cb99dfa93a9
pavan ch <pavan_fw10_057@masai.school>
2021-11-20 02:43:45 +0530

    styling changes in payment page

--- frontend/src/Components/Payment.jsx
+++ frontend/src/Components/Payment.jsx
@@ -22,46 +22,58 @@ function Payment() {
         
         <div>
             <h1>Payment Page</h1>
+            <div className={styles.MainContainer}>
             <div className={styles.box}>
-            <div><h5>Product:</h5>{product.product}</div>
-            <div><h5>Sender Name:</h5>{product.senderName}</div>
-            <div><h5>Sender Address:</h5>{product.senderAddress}</div>
-            <div><h5>Receiver Name:</h5>{product.recieverName}</div>
-            <div><h5>Reciever Address:</h5>{product.recieverAddress}</div>
-            <div><h5>PickUp Time:</h5>{product.pickupTime}</div>
-            <div><h5>Preferable Time:</h5>{product.preferableTime}</div>
-                <div><h5>Size Price:</h5>{product.sizePrice}</div>
+            <div className={styles.insideBox}><b>Details of the product</b></div>
+            <div className={styles.insideBox}><b>Product to be delivered:</b>{product.product}</div>
+            {/* <div><h5>Sender Name:</h5>{product.senderName}</div>
+            <div><h5>Sender Address:</h5>{product.senderAddress}</div> */}
+            <div className={styles.insideBox}><b>Receiver Name:</b><span>{product.recieverName}</span></div>
+            <div className={styles.insideBox}><b>Reciever Address:</b>{product.recieverAddress}</div>
+            <div className={styles.insideBox}><b>Approx pickup Time:</b>{product.pickupTime}</div>
+            <div className={styles.insideBox}><b>Apporox delivery Time:</b>{product.preferableTime}</div>
+                <div className={styles.insideBox}><b>Size and Price:</b>{product.sizePrice}</div>
             </div>
         
-            <div>
+            <div className={styles.MainCont}>
                 <h3>Pay with Credit/Debit Card</h3>
-                <p>Your card Number is kept 100% secure and only used for this transaction</p>
+                <p className={styles.txtdec}>Your card Number is kept 100% secure and only used for this transaction</p>
                 <div>
-                    <div>
-                        <p>Cards Accepted</p>
+                    <div className={styles.payAcc}>
+                        <p><b>Cards Accepted</b></p>
                     </div>
-                    <div>
+                    <div className={styles.disImg}>
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Visa_Inc._logo.svg/2560px-Visa_Inc._logo.svg.png" alt="/" />
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/MasterCard_Logo.svg/1200px-MasterCard_Logo.svg.png" alt="/" />
                     </div>
-                    <lable>Card Number:</lable>
-                    <input type="text" placeholder="Enter Card Number" />
+                    <div className={styles.det}>
+                        <div className={styles.lable}>
+                        <lable>Card Number: </lable>
+                        <input className={styles.inp1} type="text" placeholder="Enter Card Number" />
+                        </div>
                     <br />
                     <br/>
-                     <lable>Expiry Date:</lable>
-                    <input type="text" placeholder="MM/YY" />
+                    <div className={styles.lable}>
+                        <lable>Expiry Date: </lable>
+                        <input className={styles.inp2} type="text" placeholder="MM/YY" />
+                    </div>
                     <br />
                     <br />
-                    <lable>CVV:</lable>
-                    <input type="text" placeholder="Enter CVV" />
+                    <div className={styles.lable}>
+                        <lable>CVV: </lable>
+                        <input className={styles.inp3} type="text" placeholder="Enter CVV" />
+                    </div>
                     <br />
                     <br />
-                    <Link to={`/trackorder`}><button>Pay for Order</button></Link>
+                    {/* <Link to={`/trackorder`}><button className={styles.but}>Pay for Order</button></Link> */}
+                    <button className={styles.but}><Link to={`/trackorder`} style={{ textDecoration: 'none' }}>Pay Now</Link></button>
+                    </div>
                     
                 </div>
 
             </div>
 
+        </div>
         </div>
     )
 }
